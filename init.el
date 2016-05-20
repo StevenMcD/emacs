a922c36839a0e90d4242871e22adab84aa329c7c
StevenMcD <StevenMcD@gmail.com>
2016-05-20 22:53:00 +0200

    Added Monokai theme

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
+(load-theme 'monokai t)
+
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
